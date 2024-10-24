a2bc31abc8afbc86f476f0b54826721d51438313
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-25 06:56:10 +0800

    Looks: lighter bg

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gradient-to-br from-purple-200 to-indigo-200 min-h-screen">
+    <div className="bg-gradient-to-br from-purple-100 to-indigo-100 min-h-screen">
       <AppBar />
       <div className="px-4 pt-20">{children}</div>
       <Navbar />
