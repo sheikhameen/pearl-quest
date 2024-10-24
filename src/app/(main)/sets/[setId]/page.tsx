99b65487dc4aab5eb2cdb2295ea75fd990c06026
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-24 23:28:05 +0800

    Feat: initialise play set page

--- src/app/(main)/sets/[setId]/page.tsx
+++ src/app/(main)/sets/[setId]/page.tsx
@@ -1,4 +1,6 @@
+import { buttonVariants } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 
 const Page = async ({ params }: { params: Promise<{ setId: string }> }) => {
   const { setId } = await params;
@@ -12,16 +14,20 @@ const Page = async ({ params }: { params: Promise<{ setId: string }> }) => {
   if (!set) return <div>No set found</div>;
 
   return (
-    <div>
-      <div className="space-y-2">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          {set.title}
-        </h1>
-        <p className="text-xs text-muted-foreground">
-          The king, seeing how much happier his subjects were, realized the
-          error of his ways and repealed the joke tax.
-        </p>
-      </div>
+    <div className="flex flex-col gap-2 justify-center items-center h-96">
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        {set.title}
+      </h1>
+      <p className="text-xs text-muted-foreground text-center">
+        The king, seeing how much happier his subjects were, realized the error
+        of his ways and repealed the joke tax.
+      </p>
+      <Link
+        href={`/sets/${set.id}/play`}
+        className={buttonVariants({ size: "lg" })}
+      >
+        Start
+      </Link>
     </div>
   );
 };
