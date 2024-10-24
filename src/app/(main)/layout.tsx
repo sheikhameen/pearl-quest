812176bb337893c58d5c8d26177a8ab17dc3bf14
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-24 19:12:36 +0800

    Feat: add appbar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,8 +1,10 @@
+import AppBar from "@/components/appbar";
 import Navbar from "@/components/navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-gradient-to-br from-purple-200 to-indigo-200 min-h-screen">
+      <AppBar />
       {children}
       <Navbar />
     </div>
---
+++ src/components/appbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AppBar = () => {
+  return (
+    <div className="sticky top-0 bg-white p-4">
+      <div className="text-xl font-bold text-purple-700 font-mono tracking-tight">
+        Pearl Quest
+      </div>
+    </div>
+  );
+};
+
+export default AppBar;
