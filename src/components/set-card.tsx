94083075c2d6c3bea678cbe688acc5ffce607da8
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-24 22:24:45 +0800

    Looks: Change gradient color

--- src/components/set-card.tsx
+++ src/components/set-card.tsx
@@ -6,7 +6,7 @@ interface SetCardProps {
 }
 const SetCard = ({ title }: SetCardProps) => {
   return (
-    <Card className="border-none bg-gradient-to-r from-fuchsia-500 to-indigo-500 text-primary-foreground rounded-2xl shadow-xl hover:scale-95 transition-transform">
+    <Card className="border-none bg-gradient-to-r from-indigo-500 to-indigo-700 text-primary-foreground rounded-2xl shadow-xl hover:scale-95 transition-transform">
       <CardContent className="p-6 flex justify-between items-center">
         <CardTitle className="text-3xl">{title}</CardTitle>
         <Button size="lg" className="bg-primary/70">
