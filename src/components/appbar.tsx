a40fc89bee9e611aa2dc29fdee1265b6a12bf75c
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-25 15:56:28 +0800

    Feat: update new logo

--- src/components/appbar.tsx
+++ src/components/appbar.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const AppBar = () => {
   return (
     <div className="fixed top-0 inset-x-0 bg-white p-4">
-      <div className="text-xl font-bold text-purple-700 font-mono tracking-tight">
-        Pearl Quest
+      <div className="text-lg font-bold bg-gradient-to-r from-purple-600 to-purple-800 text-white w-max px-2 py-0.5 rounded-sm tracking-tight shadow-[5px_5px_0_rgba(40,4,70,1)]">
+        Pearl Quest.
       </div>
     </div>
   );
