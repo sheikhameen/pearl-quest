a0a3a3c0ad1c3730346e83276468098cf75e0f90
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-25 12:04:43 +0800

    Feat: add padding bottom to layout so content isnt blocked when longer than page

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-gradient-to-br from-purple-100 to-indigo-100 min-h-screen">
       <AppBar />
-      <div className="px-4 pt-20">{children}</div>
+      <div className="px-4 pt-20 pb-24 md:pb-28">{children}</div>
       <Navbar />
     </div>
   );
