ce6c61c40176957ca8873cc648b3d3371960ddfe
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-25 11:38:08 +0800

    Feat: add good looking form that doesnt do anything other than validate

--- src/app/(main)/sets/new/page.tsx
+++ src/app/(main)/sets/new/page.tsx
@@ -1,7 +1,109 @@
-import React from "react";
+"use client";
+
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+const formSchema = z.object({
+  key: z
+    .string()
+    .min(3, "Key must have at least 3 characters")
+    .max(20, "Key must have at most 20 characters"),
+  title: z
+    .string()
+    .min(3, "Title must have at least 3 characters")
+    .max(64, "Title must have at most 64 characters"),
+  description: z
+    .string()
+    .min(3, "Description must have at least 3 characters")
+    .max(256, "Description must have at most 256 characters"),
+});
 
 const Page = () => {
-  return <div>Page</div>;
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      key: "",
+      title: "",
+      description: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Create a New Set</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            <FormField
+              control={form.control}
+              name="key"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Key</FormLabel>
+                  <FormDescription>
+                    You need a valid key to create question sets.
+                  </FormDescription>
+                  <FormControl>
+                    <Input placeholder="You key here..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Set title here..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Set description here..." {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default Page;
