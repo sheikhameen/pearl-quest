7529c0335ce5f0994b0d08c1779894644ce9947e
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 07:47:15 +0800

    Feat: limit content width on large devices

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -5,7 +5,9 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="bg-gradient-to-br from-purple-100 to-indigo-100 min-h-screen">
       <AppBar />
-      <div className="px-4 pt-20 pb-24 md:pb-28">{children}</div>
+      <div className="container mx-auto px-4 pt-20 pb-24 md:pb-28">
+        {children}
+      </div>
       <Navbar />
     </div>
   );
