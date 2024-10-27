fb26d4aef5c0886c391c39ee0463b0be262fa5a0
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 07:55:19 +0800

    Feat: clamp set description, set minimum height for set cards

--- src/components/set-card.tsx
+++ src/components/set-card.tsx
@@ -8,10 +8,10 @@ interface SetCardProps {
 const SetCard = ({ set }: SetCardProps) => {
   return (
     <Link href={`/sets/${set.id}`} className="block">
-      <Card className="h-full border-none bg-gradient-to-br from-indigo-500 to-indigo-700 text-primary-foreground  shadow-[0_6px_0_hsl(243.7_54.5_41.4)] hover:shadow-[0_3px_0_hsl(243.7_54.5_41.4)] hover:translate-y-[3px] active:shadow-[0_0px_0_hsl(243.7_54.5_41.4)] active:translate-y-[6px]">
+      <Card className="h-full min-h-36 border-none bg-gradient-to-br from-indigo-500 to-indigo-700 text-primary-foreground  shadow-[0_6px_0_hsl(243.7_54.5_41.4)] hover:shadow-[0_3px_0_hsl(243.7_54.5_41.4)] hover:translate-y-[3px] active:shadow-[0_0px_0_hsl(243.7_54.5_41.4)] active:translate-y-[6px]">
         <CardHeader>
           <CardTitle>{set.title}</CardTitle>
-          <CardDescription className="text-muted/80">
+          <CardDescription className="text-muted/80 line-clamp-3">
             {set.description}
           </CardDescription>
         </CardHeader>
