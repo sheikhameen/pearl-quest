3dff7a6398347917be93bb65aed1bc3cba36c21b
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 07:45:11 +0800

    Feat: add credits

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,5 +1,29 @@
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+
 const Page = () => {
-  return <div>Settings Page</div>;
+  return (
+    <div>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Credits
+      </h1>
+      <Table>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-semibold text-muted-foreground">
+              Project by:
+            </TableCell>
+            <TableCell>Asif</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-semibold text-muted-foreground">
+              Class:
+            </TableCell>
+            <TableCell>Year 7 Pearl</TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </div>
+  );
 };
 
 export default Page;
