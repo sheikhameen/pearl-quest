fa370d59cd02bfd6bf996324e341f420cc11ecbb
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 12:38:40 +0800

    Feat: add question difficulty

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -11,6 +11,7 @@ import {
 import { Button } from "./ui/button";
 import { Progress } from "./ui/progress";
 import toast from "react-hot-toast";
+import { cn } from "@/lib/utils";
 
 type SetWithQuestionAndAnswers = Prisma.SetGetPayload<{
   include: { questions: { include: { answers: true } } };
@@ -24,6 +25,12 @@ const timeouts = {
   EASY: 10,
   MEDIUM: 20,
   HARD: 30,
+} as const;
+
+const points = {
+  EASY: 1,
+  MEDIUM: 2,
+  HARD: 3,
 } as const;
 
 const Game = ({ set }: GameProps) => {
@@ -43,13 +50,7 @@ const Game = ({ set }: GameProps) => {
   const handleAnswer = (answer: Answer) => {
     if (answer.isCorrect) {
       toast.success("Correct!");
-      const points =
-        currentQuestion.difficulty === "EASY"
-          ? 1
-          : currentQuestion.difficulty === "MEDIUM"
-          ? 2
-          : 3;
-      setScore((prevScore) => prevScore + points);
+      setScore((prevScore) => prevScore + points[currentQuestion.difficulty]);
     } else {
       toast.error("Wrong!");
     }
@@ -104,6 +105,7 @@ const Game = ({ set }: GameProps) => {
           value={(timeLeft / timeouts[currentQuestion.difficulty]) * 100}
           className="mb-4"
         />
+
         <p className="mb-4 text-lg font-medium">{currentQuestion.text}</p>
         <div className="grid gap-2">
           {currentQuestion.answers.map((answer) => (
@@ -118,8 +120,27 @@ const Game = ({ set }: GameProps) => {
           ))}
         </div>
       </CardContent>
-      <CardFooter>
-        <p className="text-lg font-semibold">Score: {score}</p>
+      <CardFooter className="flex justify-between items-center">
+        <p className="text-xs mb-2 font-semibold">
+          <span
+            className={cn(
+              "text-white px-1.5 py-0.5 w-max rounded",
+              currentQuestion.difficulty === "EASY"
+                ? "bg-green-600"
+                : currentQuestion.difficulty === "MEDIUM"
+                ? "bg-yellow-500"
+                : "bg-destructive"
+            )}
+          >
+            {currentQuestion.difficulty}
+          </span>{" "}
+          <span className="text-muted-foreground">
+            {points[currentQuestion.difficulty]} point
+            {currentQuestion.difficulty !== "EASY" && "s"} |{" "}
+            {timeouts[currentQuestion.difficulty]} seconds
+          </span>
+        </p>
+        <p className="text-lg font-semibold">Total Score: {score}</p>
       </CardFooter>
     </Card>
   );
