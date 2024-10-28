4d2086343657ab189b44d998c4b8d8a9ec9824d4
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 11:07:55 +0800

    Looks: bluish bg

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/components/navbar";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="bg-gradient-to-br from-purple-100 to-indigo-100 min-h-screen">
+    <div className="bg-gradient-to-br from-indigo-50 to-blue-100 min-h-screen">
       <AppBar />
       <div className="container mx-auto px-4 pt-20 pb-24 md:pb-28">
         {children}
