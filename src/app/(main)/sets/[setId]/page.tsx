68f99696bd601b69ef9a1d59796ad3a2802aead0
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 12:18:49 +0800

    Feat: redesign single set page and add questions count

--- src/app/(main)/sets/[setId]/page.tsx
+++ src/app/(main)/sets/[setId]/page.tsx
@@ -1,4 +1,11 @@
 import { buttonVariants } from "@/components/ui/button";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 import Link from "next/link";
 
@@ -9,25 +16,35 @@ const Page = async ({ params }: { params: Promise<{ setId: string }> }) => {
     where: {
       id: setId,
     },
+    include: {
+      _count: {
+        select: {
+          questions: true,
+        },
+      },
+    },
   });
 
   if (!set) return <div>No set found</div>;
 
   return (
-    <div className="flex flex-col gap-2 justify-center items-center h-96">
-      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        {set.title}
-      </h1>
-      <p className="text-xs text-muted-foreground text-center">
-        {set.description}
-      </p>
-      <Link
-        href={`/sets/${set.id}/play`}
-        className={buttonVariants({ size: "lg" })}
-      >
-        Start
-      </Link>
-    </div>
+    <Card className="flex flex-col items-center justify-center max-w-96 mx-auto">
+      <CardHeader className="text-center space-y-2">
+        <CardTitle>{set.title}</CardTitle>
+        <CardDescription>{set.description}</CardDescription>
+        <CardDescription className="text-base font-semibold">
+          {set._count.questions} questions
+        </CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <Link
+          href={`/sets/${set.id}/play`}
+          className={buttonVariants({ size: "lg" })}
+        >
+          Start
+        </Link>
+      </CardFooter>
+    </Card>
   );
 };
 
