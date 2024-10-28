dd341604a5015836c1234af7ccf607330d6684e6
Sheikh Ameen <sheikhameen123@gmail.com>
2024-10-28 08:04:54 +0800

    Feat: add loading spinner

--- src/app/(main)/sets/loading.tsx
+++ src/app/(main)/sets/loading.tsx
@@ -1,7 +1,12 @@
 import { Skeleton } from "@/components/ui/skeleton";
+import { Spinner } from "@/components/ui/spinner";
 
 const Loading = () => {
-  return <Skeleton className="w-full h-96" />;
+  return (
+    <Skeleton className="w-full h-96 flex items-center justify-center">
+      <Spinner size="large" />
+    </Skeleton>
+  );
 };
 
 export default Loading;
